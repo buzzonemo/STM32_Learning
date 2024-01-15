ca97282ee1ae04b5ccd0d20c9b1298666c007269
buzzoneone <1063839329@qq.com>
2024-01-15 16:35:24 +0800

    知识点1
    i2c发送字节的时候，如何判别高电平和低电平

---
+++ 10 I2C/record/1/demo/build/CMakeFiles/main.dir/src/hello.c.obj.d
@@ -0,0 +1,8 @@
+CMakeFiles/main.dir/src/hello.c.obj: C:/Users/mu/Desktop/x/src/hello.c \
+ d:\mingw\include\stdio.h d:\mingw\include\_mingw.h \
+ d:\mingw\include\msvcrtver.h d:\mingw\include\w32api.h \
+ d:\mingw\include\sdkddkver.h \
+ d:\mingw\lib\gcc\mingw32\6.3.0\include\stddef.h \
+ d:\mingw\include\sys/types.h \
+ d:\mingw\lib\gcc\mingw32\6.3.0\include\stdarg.h \
+ C:/Users/mu/Desktop/x/include/m.h
---
+++ 10 I2C/record/1/demo/build/CMakeFiles/main.dir/src/max.c.obj.d
@@ -0,0 +1,1 @@
+CMakeFiles/main.dir/src/max.c.obj: C:/Users/mu/Desktop/x/src/max.c
